b8d12db40b93ea4bd0bec05441895a19d821bb43
Luke Kreczko <lkreczko@googlemail.com>
2021-10-27 14:32:04 +0100

    added docstring for def convert_to_largest_unit

--- src/dice_lib/units.py
+++ src/dice_lib/units.py
@@ -4,7 +4,17 @@ from typing import Tuple
 def convert_to_largest_unit(
     value: float, unit: str, scale: float = 1000.0
 ) -> Tuple[float, str]:
-    """Converts value to largest unit of the same type"""
+    """Converts value to largest unit of the same type.
+    Largest unit is the largest unit where value < scale
+
+    E.g.
+        with scale = 1000.0, unit='m' and value=1.0:
+        1.0 m -> 1.0 m
+
+        with scale = 1000.0, unit='m' and value=231234.0:
+        231234.0 m -> 231.234 km
+
+    """
     prefixes = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"]
     current_scale = value
     prefix_index = 0
