0a17affb4b3def253704ebeb522de80b08202e89
kreczko <lkreczko@googlemail.com>
2022-03-10 17:13:03 +0000

    added stubs for distributed and network

---
+++ src/dice_lib/distributed/__init__.py
@@ -0,0 +1,8 @@
+"""
+The place for Dask, joblib, ray, and connectors to other distributed systems.
+
+ - Dask: https://dask.org/
+ - joblib: https://joblib.readthedocs.io/en/latest/
+ - ray: https://ray.readthedocs.io/en/latest/
+ - tune: https://docs.ray.io/en/latest/tune/index.html
+"""
