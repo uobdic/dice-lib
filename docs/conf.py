68bcd1a885a18cf6df951beab251c0769fefedde
kreczko <lkreczko@googlemail.com>
2023-11-28 13:19:22 +0000

    docs: update template

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,11 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
     "sphinx_copybutton",
+    "sphinxcontrib.mermaid",
 ]
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
@@ -35,7 +38,14 @@ templates_path = []
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", ".env"]
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -43,21 +53,38 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "furo"
 
 html_title = f"{project}"
 
 html_baseurl = "https://dice_lib.readthedocs.io/en/latest/"
 
 html_theme_options = {
-    "home_page_in_toc": True,
-    "repository_url": "https://github.com/uobdic/dice-lib",
-    "use_repository_button": True,
-    "use_issues_button": True,
-    "use_edit_page_button": True,
+    "source_repository": "https://github.com/uobdic/dice-lib",
+    "source_branch": "main",
+    "source_directory": "docs/",
+    "light_logo": "dice_logo_light.png",
+    "dark_logo": "dice_logo_dark.png",
+    "announcement": 'Interested in contributing? Head to the <a href="https://github.com/uobdic/dice-lib">GitHub repository</a> and open a pull request!',
 }
+html_css_files = ["css/custom.css"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path: List[str] = []
+html_static_path: List[str] = ['_static']
+
+myst_enable_extensions = [
+    "colon_fence",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+nitpick_ignore = [
+    ("py:class", "_io.StringIO"),
+    ("py:class", "_io.BytesIO"),
+]
+
+always_document_param_types = True
